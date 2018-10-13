1817e7b3cd083c5ee6aa9b9b742abf691303c5d0
Louay Nasr <LouayNasr@users.noreply.github.com>
2018-10-13 16:24:22 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+function newQuoteMethod() {
+    "use strict";
+
+    var text = document.getElementById("text");
+    var author = document.getElementById('author');
+
+
+    var quotes = [
+        ["Stay Hungry. Stay Foolish.", "Steve Jobs"],
+        ["Good Artists Copy, Great Artists Steal.", "Pablo Picasso"],
+        ["Argue with idiots, and you become an idiot.", "Paul Graham"],
+        ["Be yourself; everyone else is already taken.", "Oscar Wilde"],
+        ["Simplicity is the ultimate sophistication.", "Leonardo Da Vinci"]
+    ];
+    var randomLine = Math.floor(Math.random() * quotes.length);
+    text.innerHTML = quotes[randomLine][0];
+    author.innerHTML = quotes[randomLine][1];
+    
+};
+newQuoteMethod();
